37e443c87985e74dbe38393123cb0d284ea13ebd
Francescaparty <Francescaparticelli6@gmail.com>
2025-02-24 12:40:23 +0100

    importing 1240
    adding a new import

--- regression.py
+++ regression.py
@@ -2,6 +2,7 @@
 import pandas as pd # for DataFrames
 import numpy as np # for numpy array operations
 import matplotlib.pyplot as plt # for visualization
+import seaborn as sns
 # importing all the needed functions from scikit-learn
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import PolynomialFeatures, StandardScaler
@@ -10,9 +11,21 @@ from sklearn.metrics import mean_squared_error
 # --- END OF IMPORT SECTION ---
 
 # --- MAIN CODE ---
+#importing the dataset
 url = "Salary_Data.csv"
 data = pd.read_csv(url)
-print(data.head())
+
+#Visualizing the dataset
+print(f"\nHere are the first 5 rows of the dataset:\n{data.head()}")
+
+#Using a plot to visualize the data
+plt.title("Years of Experience vs Salary") #title for the plot
+plt.xlabel("Years of Experience") # title of x axis
+plt.ylabel("Salary") # title of y axis
+plt.scatter(data["YearsExperience"], data["Salary"], color="blue") # actual plot
+sns.regplot(data= data, x= "YearsExperience", y = "Salary") #regression line
+plt.show() # renderize the plot to show it
+
 
 
 # --- END OF MAIN CODE ---
