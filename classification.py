04b0d0a5af8f2a0dd42a9249cb43b98299c40661
9ineTails <Francescaparticelli6@gmail.com>
2025-02-26 09:32:09 +0100

    new file 0931
    adding a new file with new features

---
+++ classification.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd # for DataFrames
+import matplotlib.pyplot as plt # to visualise the data
+from sklearn.model_selection import train_test_split # to split the data into training and testing sets
+from sklearn.preprocessing import StandardScaler # to standardize (scale) the features (the X)
+from sklearn.ensemble import RandomForestClassifier # to use the RandomForest model
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix # to evaluate the model
